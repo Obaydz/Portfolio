f9b0998e6eb3dfc70a02a4d67ca19695f97d1d78
Oubeidallah Zmander <80208961+Obaydz@users.noreply.github.com>
2025-08-26 13:56:17 +0100

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,7 +11,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Obayd | Personal Portfolio",
-  description: "Ricardo is a full-stack developer with 8 years of experience.",
+  description: "Obayd is a full-stack developer with 8 years of experience.",
 };
 
 export default function RootLayout({
