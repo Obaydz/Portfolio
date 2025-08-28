f0e93ef21a23af41704f2e05b458c0279033f64a
Oubeidallah Zmander <80208961+Obaydz@users.noreply.github.com>
2025-08-28 22:46:04 +0100

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,11 @@ import Intro from "@/components/intro";
 import Projects from "@/components/projects";
 import SectionDivider from "@/components/section-divider";
 import Skills from "@/components/skills";
+import { Analytics } from "@vercel/analytics/next"
 
 export default function Home() {
   return (
+    <Analytics/>
     <main className="flex flex-col items-center px-4">
       <Intro />
       <SectionDivider />
