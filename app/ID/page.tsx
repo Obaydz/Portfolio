c67dd46756989dd8ee06f6dd571653e3475bb76e
Oubeidallah Zmander <80208961+Obaydz@users.noreply.github.com>
2025-09-07 17:13:32 +0100

    Delete app/ID directory

--- app/ID/page.tsx
+++
@@ -1,9 +0,0 @@
-import IdentityCard from "@/components/IdentityCard";
-
-export default function Page() {
-  return (
-    <main className="flex flex-col items-center justify-center min-h-screen p-4">
-      <IdentityCard />
-    </main>
-  );
-}
