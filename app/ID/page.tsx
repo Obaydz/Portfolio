733df163eafc794a3de6a17d64c4f6c714427903
Oubeidallah Zmander <80208961+Obaydz@users.noreply.github.com>
2025-09-07 15:55:59 +0100

    Add files via upload

---
+++ app/ID/page.tsx
@@ -0,0 +1,9 @@
+import IdentityCard from "@/components/IdentityCard";
+
+export default function Page() {
+  return (
+    <main className="flex flex-col items-center justify-center min-h-screen p-4">
+      <IdentityCard />
+    </main>
+  );
+}
